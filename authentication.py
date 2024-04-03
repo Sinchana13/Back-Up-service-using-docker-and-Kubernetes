bcc65fa08b19467737fb772516925d3eec76a918
SINCHANA S <sinchanas1308@gmail.com>
2024-04-03 12:53:46 +0530

    Initial commit

---
+++ authentication.py
@@ -0,0 +1,20 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.auth.credentials import Credentials  # Add this import statement
+import os.path
+
+def authenticate():
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    return creds
